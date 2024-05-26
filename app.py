406c54e1aa1cb8da0034ce7ecd383bb26eea882d
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-26 20:26:40 -0300

    a

--- app.py
+++ app.py
@@ -99,6 +99,15 @@ containerborda = """
 """
 st.markdown(containerborda,unsafe_allow_html=True)
 
-
+containerborda = """
+    <style>
+    [p]
+    {
+    font-size: 30pt;
+    text-align: center;
+    }
+    </style>
+"""
+st.markdown(containerborda,unsafe_allow_html=True)
     
 
