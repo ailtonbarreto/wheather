af108afc9a97d8c1f7faa49df1eb5311aead58cf
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-26 18:01:21 -0300

    a

--- app.py
+++ app.py
@@ -62,53 +62,9 @@ except KeyError:
 temp = f'{int(temperatura)}°C'
 
 with container:
-    if cidade == "":
-        st.subheader("digite uma cidade",anchor=False)
-    if descricao == "nublado":
-        st.title(temp,anchor=False)
-        st.image("nublado.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "chuva moderada":
-        st.title(temp,anchor=False)
-        st.image("chuva moderada.png",use_column_width=True)
-        st.title(descricao,anchor=False) 
-    if descricao == "ensolarado":
-        st.title(temp,anchor=False)
-        st.image("ensolarado.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "névoa":
-        st.title(temp,anchor=False)
-        st.image("nevoa.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "céu limpo" and hora >= noite:
-        st.title(temp,anchor=False)
-        st.image("lua céu claro.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "nuvens dispersas": 
-        st.title(temp,anchor=False)
-        st.image("nuvens dispersas.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "chuva leve": 
-        st.title(temp,anchor=False)    
-        st.image("chuva leve.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "céu limpo" and hora <= noite:
-        st.title(temp,anchor=False)
-        st.image("ensolarado.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "algumas nuvens" and hora <= noite:
-        st.title(temp,anchor=False)
-        st.image("poucas nuvens dia.png",use_column_width=True)
-        st.title(descricao,anchor=False)
-    if descricao == "algumas nuvens" and hora >= noite:
-         st.title(temp,anchor=False)
-         st.image("poucas nuvens noite.png",use_column_width=True)
-         st.title(descricao,anchor=False)
-    if descricao == "Cidade não encontrada" and cidade != "":
-        st.image("not-found.png",use_column_width=True)
-        st.subheader("Cidade não encontrada",anchor=False)
-    
-    st.write(link)
+    print(link)
+
+
 #--------------------------------------------------------------------------------
     
 desativardetalhes = """
